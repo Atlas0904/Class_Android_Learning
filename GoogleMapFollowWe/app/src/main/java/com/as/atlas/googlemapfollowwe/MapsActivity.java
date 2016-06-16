5685766372f3c7cf3300ce35bc3d024e1210bc60
atlas0904 <u920243@gmail.com>
2016-06-16 19:18:44 +0800

    [Class][Android] Google Map Follow WE - update function to get current location (NTDebug)
    Title:
    Type:
    Root Cause:
    Solution:
    Feature:

--- GoogleMapFollowWe/app/src/main/java/com/as/atlas/googlemapfollowwe/MapsActivity.java
+++ GoogleMapFollowWe/app/src/main/java/com/as/atlas/googlemapfollowwe/MapsActivity.java
@@ -1,8 +1,21 @@
 package com.as.atlas.googlemapfollowwe;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,11 +24,27 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
+
+
+    private GoogleApiClient googleApiClient;
+
+    // Location請求物件
+    private LocationRequest locationRequest;
+
+    // 記錄目前最新的位置
+    private Location currentLocation;
+
+    // 顯示目前與儲存位置的標記物件
+    private Marker currentMarker, itemMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +54,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        configGoogleApiClient();
+        configLocationRequest();
     }
 
 
@@ -49,13 +81,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         */
 
+
+        /*
         // 建立位置的座標物件
         LatLng place = new LatLng(25.033408, 121.564099);
         // 移動地圖
         moveMap(place);
         addMarker(place, "Hello!", " Google Maps v2!");
-
+*/
     }
+
     // 移動地圖到參數指定的位置
     private void moveMap(LatLng place) {
         // 建立地圖攝影機的位置物件
@@ -68,6 +103,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // 使用動畫的效果移動地圖
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
+
     // 在地圖加入指定位置與標題的標記
     private void addMarker(LatLng place, String title, String snippet) {
         BitmapDescriptor icon =
@@ -80,5 +116,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(icon);
 
         mMap.addMarker(markerOptions);
+    }
+
+    private void configGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).
+                addApi(LocationServices.API).
+                build();
+    }
+
+    private void configLocationRequest() {
+        locationRequest = new LocationRequest();
+        // 設定讀取位置資訊的間隔時間為一秒（1000ms）
+        locationRequest.setInterval(1000);
+        // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
+        locationRequest.setFastestInterval(1000);
+        // 設定優先讀取高精確度的位置資訊（GPS）
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // 已經連線到Google Services
+        // 啟動位置更新服務
+        // 位置資訊更新的時候，應用程式會自動呼叫LocationListener.onLocationChanged
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            Log.d("Atlas", "permission check failure.");
+//            return;
+//        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApiClient, locationRequest, MapsActivity.this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Google Services連線中斷
+        // int參數是連線中斷的代號
+
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // Google Services連線失敗
+        // ConnectionResult參數是連線失敗的資訊
+
+        // ConnectionResult參數是連線失敗的資訊
+        int errorCode = connectionResult.getErrorCode();
+
+        // 裝置沒有安裝Google Play服務
+        if (errorCode == ConnectionResult.SERVICE_MISSING) {
+            Toast.makeText(this, R.string.google_play_service_missing,
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // 位置改變
+        // Location參數是目前的位置
+
+        // 位置改變
+        // Location參數是目前的位置
+        currentLocation = location;
+        LatLng latLng = new LatLng(
+                location.getLatitude(), location.getLongitude());
+
+        // 設定目前位置的標記
+        if (currentMarker == null) {
+            currentMarker = mMap.addMarker(new MarkerOptions().position(latLng));
+        }
+        else {
+            currentMarker.setPosition(latLng);
+        }
+
+        // 移動地圖到目前的位置
+        moveMap(latLng);
     }
 }
